022237775b9888bc597f1a41e56b535d4254fedb
ninginthecloud <liningblack@hotmail.com>
2014-12-16 12:16:38 -0800

    something

--- gaussian.r
+++ gaussian.r
@@ -64,19 +64,20 @@ library(ggplot2)
 library(gridExtra)
 library(cluster)
 library(mclust) 
+####################
 #read data
+####################
 ## read two images, one forecast and one observation
 observation.table = read.table(file="E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\coamps_ttl_prcp_g2_2005051100_f24.txt_nohead.gz", header = F)
-
-
-#
-candidates<-c(paste("f0",c(7:9),sep=""),paste("f0",c(10:15),sep=""))
-filename<-paste("E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\coamps_ttl_prcp_g2_2005051100_",candidates,".txt_nohead.gz",sep="")
-
+colnames(observation.table)<-c("lat","long","z")
+##################
+#candidates<-c(paste("f0",c(7:9),sep=""),paste("f0",c(10:15),sep=""))
+#filename<-paste("E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\coamps_ttl_prcp_g2_2005051100_",candidates,".txt_nohead.gz",sep="")
 #for(i in 1:length(candidates)){
 #i=1
 #forecast.table=read.table(file=filename[i],header=F)
-forecast.table = read.table(file="E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\coamps_ttl_prcp_g2_2005051100_f14.txt_nohead.gz", header = F)
+#################
+forecast.table = read.table(file="E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\coamps_ttl_prcp_g2_2005051100_f09.txt_nohead.gz", header = F)
 colnames(forecast.table)<-c("lat","long","z")
 #cluster
 #combine data together
@@ -91,14 +92,20 @@ wkdata$z.mod<-wkdata$z
 wkdata$z.mod[wkdata$z<threshold]=0
 #modify data
 wkdata$binary<-as.numeric(wkdata$z.mod>0)
+####################
+#plot raw
+####################
 raw<-ggplot(data=wkdata,aes(x=long,y=lat,colour=z.mod))+geom_point()
 pdf<-pdf("raw_f14.pdf",width=8,height=6)
 raw
 dev.off()
 
+#######################
+#cluster test
+#######################
 try<-cbind(wkdata$long[wkdata$z>threshold],wkdata$lat[wkdata$z>threshold],wkdata$z.mod[wkdata$z>threshold])
 set.seed(570)
-test<-try[sample(dim(try)[1], size=5000, replace = FALSE),1:2]
+test<-try[sample(dim(try)[1], size=25000, replace = FALSE),1:2]
 #default<-mclustBIC(test)
 #testsummary<-summary(default,test)
 #pdf<-pdf("cluster_f14.pdf",width=8,height=6)
@@ -107,5 +114,38 @@ test<-try[sample(dim(try)[1], size=5000, replace = FALSE),1:2]
 #}
 
 #####
-model<-Mclust(data=test,G=9,modelNames="VVV")
-measure(model,G=9)
+#get 95 quantile
+#####
+filenames <- list.files(path="E:\\UW\\autumn 2014\\RA\\code\\Data\\DATA_COAMPS\\", pattern="*nohead.gz", full.names=TRUE)
+length(filenames)#98
+temp<-NULL;
+for(i in filenames){
+temp<-c(temp,as.vector(t(read.table(file=i,colClasses=c("NULL","NULL","numeric"), header = F))))
+}
+quantile(temp,probs=0.95)#18.34836
+quan<-NULL;
+for(i in filenames){
+temp<-as.vector(t(read.table(file=i,colClasses=c("NULL","NULL","numeric"), header = F)))
+quan<-c(quan,quantile(temp,probs=0.95))
+}
+median(quan)# 17.30275
+
+#####
+#no prior
+model1<-Mclust(data=test,G=9,modelNames="VVV")
+plot(model1,what="classification")
+measure(model1,G=9)
+#prior
+model2<-Mclust(data=test,G=9,modelNames="VVV",prior= priorControl())
+measure(model2,G=9)
+#no difference
+model3<-Mclust(data=test,G=9,modelNames="VEV",prior= priorControl())
+model4<-Mclust(data=test,G=9,modelNames="EEV",prior= priorControl())
+
+pdf<-pdf("diff model.pdf",width=8,height=6)
+plot(model1,what="classification")
+plot(model2,what="classification")
+plot(model3,what="classification")
+plot(model4,what="classification")
+dev.off()
+
